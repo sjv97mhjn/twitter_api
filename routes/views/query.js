ec37b981afb433296da6edb7c52ebdc329cc68c8
Sajeev Mahajan <sjv97mhjn@gmail.com>
2018-03-05 18:44:18 +0530

    Project Complete -V2

--- routes/views/query.js
+++ routes/views/query.js
@@ -7,25 +7,45 @@ var favorite_count_greaterThan = -1;
 var favorite_count_lessThan = Number.MAX_SAFE_INTEGER;
 var tweet_text = new RegExp();
 var tweet_screen_name = new RegExp();
-var Sort ;
-var Tweet_Screen_Name ={};
+var Sort;
+var Tweet_Screen_Name = {};
 var Tweet_Text = {};
-var Limit =5;
-var Skip =0;
-var page= 0;
+var Limit = 5;
+var Skip = 0;
+var page = 0;
+
+var initialize = function(){
+  mongo_tweet = require("../../models/tweet");
+  start = new Date(2012, 7, 4);
+  end = Date.now();
+  retweet_count_greaterThan = -1;
+  retweet_count_lessThan = Number.MAX_SAFE_INTEGER;
+  favorite_count_greaterThan = -1;
+  favorite_count_lessThan = Number.MAX_SAFE_INTEGER;
+  tweet_text = new RegExp();
+  tweet_screen_name = new RegExp();
+  Sort;
+  Tweet_Screen_Name = {};
+  Tweet_Text = {};
+  Limit = 5;
+  Skip = 0;
+  page = 0;
+}
 module.exports = {
   Filter: function(req, res) {
     res.render("filter.ejs");
   },
   filter: function(req, res) {
-    if(req.params.page)
-     page = Number(req.params.page);
-     if(page<0)
-     page=0;
+    if(req.query.check){
+      initialize();
+    }
+
+    if (req.params.page) page = Number(req.params.page);
+    if (page < 0) page = 0;
     console.log(req.query);
 
-  if(req.query.text){
-    Sort = `${req.query.sort}`;
+    if (req.query.sort) Sort = `${req.query.sort}`;
+
     if (req.query.retweetMin) retweet_count_greaterThan = req.query.retweetMin;
 
     if (req.query.retweetMax) retweet_count_lessThan = req.query.retweetMax;
@@ -38,29 +58,29 @@ module.exports = {
     if (req.query.start) start = req.query.start;
 
     if (req.query.end) end = req.query.end;
-
-    var Tweet_Text = {
-      contain: new RegExp(req.query.text, "i"),
-      start: new RegExp("^" + req.query.text, "i"),
-      end: new RegExp(req.query.text + "$", "i"),
-      exact: req.query.text
-    };
+    if (req.query.text) {
+      var Tweet_Text = {
+        contain: new RegExp(req.query.text, "i"),
+        start: new RegExp("^" + req.query.text, "i"),
+        end: new RegExp(req.query.text + "$", "i"),
+        exact: req.query.text
+      };
+    }
 
     if (req.query.text && req.query.tweet_type)
       tweet_text = Tweet_Text[req.query.tweet_type];
 
-
-    var Tweet_Screen_Name = {
-      contain: new RegExp(req.query.screen_name, "i"),
-      start: new RegExp("^" + req.query.screen_name, "i"),
-      end: new RegExp(req.query.screen_name + "$", "i"),
-      exact: req.query.screen_name
-    };
+    if (req.query.screen_name) {
+      var Tweet_Screen_Name = {
+        contain: new RegExp(req.query.screen_name, "i"),
+        start: new RegExp("^" + req.query.screen_name, "i"),
+        end: new RegExp(req.query.screen_name + "$", "i"),
+        exact: req.query.screen_name
+      };
+    }
 
     if (req.query.screen_name && req.query.screen_name_type)
       tweet_screen_name = Tweet_Screen_Name[req.query.screen_name_type];
-  }
-
 
     console.log(tweet_text);
     console.log(tweet_screen_name);
@@ -78,23 +98,27 @@ module.exports = {
         $lt: favorite_count_lessThan
       }
     };
+    console.log(query);
     mongo_tweet
       .find(query)
       .sort(Sort)
       .limit(Limit)
-      .skip(page*Limit)
+      .skip(page * Limit)
       .exec(function(err, results) {
         if (err) console.log(err);
         else {
           ParsedResult = csvParser(results);
           //console.log(results);
           //res.send(ParsedResult);
-          res.render("filteredTweets.ejs", { Data: ParsedResult ,data:results,page:page});
+          res.render("filteredTweets.ejs", {
+            Data: ParsedResult,
+            data: results,
+            page: page
+          });
         }
       });
   },
-  downloadcsv : function(req,res){
-
+  downloadcsv: function(req, res) {
     var query = {
       text: tweet_text,
       screen_name: tweet_screen_name,
@@ -117,12 +141,10 @@ module.exports = {
           ParsedResult = csvParser(results);
           //console.log(results);
           //res.send(ParsedResult);
-          res.render("csv.ejs", { Data: ParsedResult});
+          res.render("csv.ejs", { Data: ParsedResult });
         }
       });
-
   }
-
 };
 
 function csvParser(Data) {
